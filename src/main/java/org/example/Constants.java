84a3e2aedfa5f99b4ea46e2a172b18984310ea41
BenSerg <bendrysevsergej@gmail.com>
2022-10-25 19:41:17 +0300

    added LoginPage and Constants classes

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public abstract class Constants
+{
+  private Constants()
+  {
+  }
+
+  public static final String DRIVER_NAME="webdriver.gecko.driver";
+  public static final String DRIVER_PATH="/home/serg/driver/geckodriver";
+  public static final String LOGIN_PAGE_TITTLE="Социальная сеть Одноклассники. Общение с друзьями в ОК. Ваше место встречи с одноклассниками";
+  public static final String URL="https://ok.ru/";
+  public static final String LOGIN_RECEIVER="technoPol30";
+  public static final String LOGIN_SENDER="technoPol2";
+  public static final String PASSWORD_RECEIVER="technoPolis2022";
+  public static final String PASSWORD_SENDER="technoPolis2022";
+  public static final String USERNAME_RECEIVER="Bot Receiver";
+  public static final String USERNAME_SENDER="Bot Sender";
+}
---
+++ src/main/java/org/example/LoginPage.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.example.Constants.LOGIN_PAGE_TITTLE;
+
+public class LoginPage
+{
+  By usernameLocator_=By.xpath("//*[@id=\"field_email\"]");
+  By passwordLocator_=By.xpath("//*[@id=\"field_password\"]");
+  By loginButtonLocator_=By.xpath("//*[@value='Войти в Одноклассники']");
+
+  private final WebDriver driver_;
+
+  public LoginPage(WebDriver driver)
+  {
+    this.driver_=driver;
+    if (!LOGIN_PAGE_TITTLE.equals(driver_.getTitle()))
+    {
+      throw new IllegalStateException("Not a login page");
+    }
+  }
+
+  public void typeUsername(String username)
+  {
+    driver_.findElement(usernameLocator_).sendKeys(username);
+  }
+
+  public void typePassword(String password)
+  {
+    driver_.findElement(passwordLocator_).sendKeys(password);
+  }
+
+  public UserPage submitLogin()
+  {
+    driver_.findElement(loginButtonLocator_).submit();
+    return new HomePage(driver_);
+  }
+
+  public UserPage loginAs(String username, String password)
+  {
+    typeUsername(username);
+    typePassword(password);
+    return submitLogin();
+  }
+}
